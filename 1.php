237e8a919c1ae6cb30aa66b5c04ba692ea6babfe
frances <1934390317@qq.com>
2018-07-25 19:01:51 +0800

    修改phpredis扩展测试文件

--- 1.php
+++ 1.php
@@ -1,14 +1,8 @@
 <?php
-
-
-      $redis= newRedis();
-
-      $redis->connect('127.0.0.1',6379);
-
-
-      $redis->set('name','xxx');
-
-
-      echo$redis->get('name');
-
+// get instance
+$redis = new Redis();
+// connect to redis server
+$redis->open('localhost',6379);
+$redis->set('user:userid:9:username','wangwu');
+var_dump($redis->get('user:userid:9:username'));
 ?>
