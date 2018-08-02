0e1f90352a3ca5bc325898e0684f4ff1fab6668a
frances <1934390317@qq.com>
2018-08-03 02:58:07 +0800

    微博index.php初始化

---
+++ lib.php
@@ -0,0 +1,35 @@
+<?php
+function P($key){
+    return $_POST[$key];
+}
+function errer($msg){
+    include "head.php";
+    echo $msg;
+    include "food.php";
+    exit();
+}
+function success($msg){
+    include "head.php";
+    echo $msg;
+    include "food.php";
+    exit();
+}
+/*
+  $redis = new Redis();
+  $redis->connect('127.0.0.1',6379,1);//短链接，本地host，端口为6379，超过1秒放弃链接
+  $redis->open('127.0.0.1',6379,1);//短链接(同上)
+  $redis->pconnect('127.0.0.1',6379,1);//长链接，本地host，端口为6379，超过1秒放弃链接
+  $redis->popen('127.0.0.1',6379,1);//长链接(同上)
+  $redis->auth('password');//登录验证密码，返回【true | false】
+  $redis->close();//释放资源
+  $redis->ping(); //检查是否还再链接,[+pong]
+ */
+function connectredis(){
+    static $redisconnect=NULL;
+    if($redisconnect!==NULL){
+        return $redisconnect;
+    }
+    $redisconnect = new Redis();
+    $redisconnect->connect('localhost',6379,1);
+    return $redisconnect;
+}
---
+++ register.php
@@ -0,0 +1,23 @@
+<?php
+include 'lib.php';
+$username=P("username");
+$password=P("password");
+$password2=P("password2");
+
+if(!$username || !$password || !$password2){
+    errer("请输入完整的注册信息！");
+}
+
+if($password!=$password2){
+    errer("两次密码不一致！");
+}
+
+$redisconnect=connectredis();
+if($redisconnect->get("user:username".$username.":userid")){
+    errer("您输入的用户名已存在，请重新输入其他用户名！");
+}
+
+$userid=$redisconnect->incr("global.userid");
+$redisconnect->set("user:userid:".$userid.":username",$username);
+$redisconnect->set("user:userid:".$userid.":password",md5($password));
+success("注册成功！");
