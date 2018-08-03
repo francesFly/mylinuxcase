302832589d672bcbd8264f4cd3fba7f735c3d3c6
frances <1934390317@qq.com>
2018-08-03 19:22:30 +0800

    微博登录功能

--- register.php
+++ register.php
@@ -1,28 +1,28 @@
 <?php
-include 'lib.php';
+include "lib.php";
 if(islogin()){
-    header('location:home.php');exit();
+    header("location:home.php");exit();
 }
-$username=P('username');
-$password=P('password');
-$password2=P('password2");
+$username=P("username");
+$password=P("password");
+$password2=P("password2");
 
 if(!$username || !$password || !$password2){
-    errer('请输入完整的注册信息！');
+    errer("请输入完整的注册信息！");
 }
 
 if($password!=$password2){
-    errer('两次密码不一致！');
+    errer("两次密码不一致！");
 }
 
 $redisconnect=connectredis();
-if($redisconnect->get('user:username:'.$username.':userid')){
-    errer('您输入的用户名已存在，请重新输入其他用户名！');
+if($redisconnect->get("user:username:".$username.":userid")){
+    errer("您输入的用户名已存在，请重新输入其他用户名！");
 }
 
-$userid=$redisconnect->incr('global.userid');
-//$redisconnect->set('user:userid:'.$userid.':userid',$userid);
-$redisconnect->set('user:userid:'.$userid.':username',$username);
-$redisconnect->set('user:userid:'.$userid.':password',md5($password));
-$redisconnect->set('user:username:'.$username.':userid',$userid);
-success('注册成功！');
+$userid=$redisconnect->incr("global.userid");
+//$redisconnect->set("user:userid:".$userid.":userid",$userid);
+$redisconnect->set("user:userid:".$userid.":username",$username);
+$redisconnect->set("user:userid:".$userid.":password",md5($password));
+$redisconnect->set("user:username:".$username.":userid",$userid);
+success("注册成功！");
