893c6ff2e5736cb5ef0c93dc5c6406682d7b0765
frances <1934390317@qq.com>
2018-08-05 03:06:19 +0800

    微博数据保存到数据库

--- insertdb.php
+++ insertdb.php
@@ -1,9 +1,7 @@
 <?php
 include "lib.php";
 $redisconnect=connectredis();
-if($redisconnect->lLen('global:newsdatas')){
-$conn=connectmysql();
-}
+
 $sql="insert into post ('postid','contents','times','userid','username') values";
 $i=0;
 while ($redisconnect->lLen('global:newsdatas') && $i++<1000){    
@@ -18,16 +16,26 @@ if($i==0){
 }
 
 $sql=substr($sql, 0,-1);
-//print_r($sql);
-/* $return=mysql_query($sql,$conn); */
-$return=$conn->query($sql);
+$mysqli = new mysqli("127.0.0.1", "root", "root", "weibodb");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    //导入到global:newsdatas中
+    foreach ($redisconnect->lrange('global:bakpastid',0,-1) as $key=>$values){
+        $postid=$redisconnect->lPop('global:bakpastid');
+        $redisconnect->rPush('global:newsdatas',$postid);
+    }
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+$return=$mysqli->query($sql);
 if(!$return){
     //导入到global:newsdatas中
     foreach ($redisconnect->lrange('global:bakpastid',0,-1) as $key=>$values){
         $postid=$redisconnect->lPop('global:bakpastid');
         $redisconnect->rPush('global:newsdatas',$postid);
     }
-    die('no insert: ' . $conn->error());
+    printf("Error: %s\n", $mysqli->error);
 }else{
     $redisconnect->del('global:bakpastid');
      echo 'ok';exit();
