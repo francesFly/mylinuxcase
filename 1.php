37109c7f0553a51629dd2b4a9d6c8905e7592cd1
frances <1934390317@qq.com>
2018-08-05 02:24:12 +0800

    微博数据保存到数据库

--- 1.php
+++ 1.php
@@ -1,8 +1,3 @@
 <?php
-// get instance
-$redis = new Redis();
-// connect to redis server
-$redis->open('localhost',6379);
-$redis->set('user:userid:9:username','wangwu');
-var_dump($redis->get('user:userid:9:username'));
+echo phpinfo();
 ?>
