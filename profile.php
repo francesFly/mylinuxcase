2fd12a3ae4b4604777a7203802b271c3fdc1bacb
frances <1934390317@qq.com>
2018-08-06 16:22:14 +0800

    微博发布信息完善

--- profile.php
+++ profile.php
@@ -15,19 +15,26 @@ if(!$currentid){
 }
 $isfriending=$redisconnect->sismember('friending:'.$logininfo['userid'],$currentid);
 $friendingstatus=$isfriending?'取消关注':'立即关注';
+//分页
+$allpage=ceil($redisconnect->lLen('mynewdatas:'.$currentid)/20);
+$pagenum=G('pagenum')?G('pagenum'):1;
+$pagestart=($pagenum-1)*20;
+$pageend=$pagenum*20-1;
+$newnumlist=$redisconnect->lrange('mynewdatas:'.$currentid,$pagestart,$pageend);
+//sort($newnumlist,SORT_NUMERIC);
+//$newnumlist=$redisconnect->sort('mynewdatas:'.$logininfo['userid'],array('sort'=>'desc'));
 ?>
 <h2 class="username"><?php echo $currentname;?></h2>
 <a href="follow.php?uid=<?php echo $currentid;?>&f=<?php echo $isfriending; ?>" class="button"><?php echo $friendingstatus; ?></a>
 
-<div class="post">
-<a class="username" href="profile.php?u=test">test</a> 
-world<br>
-<i>11 分钟前 通过 web发布</i>
-</div>
+<?php /* foreach ($newlist as $key=>$values){ */
+foreach ($newnumlist as $key=>$values){
+    $newinfo=$redisconnect->hMget('post:postid:'.$values,array('username','contents','times'));
+?>
 
 <div class="post">
-<a class="username" href="profile.php?u=test">test</a>
-hello<br>
-<i>22 分钟前 通过 web发布</i>
+<a class="username" href="profile.php?u=<?php echo $newinfo['username'];?>"><?php echo $newinfo['username'];?></a> <?php echo $newinfo['contents'];?><br>
+<i><?php echo calculatetime($newinfo['times']);?>前 通过 web发布</i>
 </div>
+<?php }?>
 <?php include 'food.php';?>
--- timeline.php
+++ timeline.php
@@ -8,7 +8,6 @@ $redisconnect=connectredis();
 $newuserlist=array();
 $newuserlist=$redisconnect->sort('newuserlink',array('sort'=>'desc','get'=>'user:userid:*:username'));
 
-
 ?>
 <h2>热点</h2>
 <i>最新注册用户(redis中的sort用法)</i><br>
@@ -19,15 +18,17 @@ $newuserlist=$redisconnect->sort('newuserlink',array('sort'=>'desc','get'=>'user
 </div>
 
 <br><i>最新的50条微博!</i><br>
-<div class="post">
-<a class="username" href="profile.php?u=test">test</a>
-world<br>
-<i>22 分钟前 通过 web发布</i>
-</div>
+<?php 
+for ($i=0;$i<50;$i++){
+    $postid=$redisconnect->get('global:postid')-$i;
+    $newinfo=$redisconnect->hMget('post:postid:'.$postid,array('username','contents','times'));
+?>
 
 <div class="post">
-<a class="username" href="profile.php?u=test">test</a>
-hello<br>
-<i>22 分钟前 通过 web发布</i>
+<a class="username" href="profile.php?u=<?php echo $newinfo['username'];?>"><?php echo $newinfo['username'];?></a> <?php echo $newinfo['contents'];?><br>
+<i><?php echo calculatetime($newinfo['times']);?>前 通过 web发布</i>
 </div>
+<?php }?>
+
+
 <?php include 'food.php';?>
